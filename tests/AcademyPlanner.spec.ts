95c875297ab6340c89a91ffc14c18d0d747294de
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-03-28 17:09:18 +0100

    Save

--- tests/AcademyPlanner.spec.ts
+++ tests/AcademyPlanner.spec.ts
@@ -1,8 +1,16 @@
 import { expect, test } from './my-test';
+import { Guid } from 'guid-typescript'
 
 test('Verify Authentication', async ({valoriAcademyPlanner}) => {
     const responseShow = await valoriAcademyPlanner.GetTrainingDetails('Acceptatietest')
     expect(responseShow.status()).toBe(200);
     const ResponseBodyDetails = await responseShow.text();
     const ResponseBodyDetailsJson = JSON.parse(ResponseBodyDetails);
+    console.log(ResponseBodyDetails)
+    expect(ResponseBodyDetailsJson.Naam).toEqual('Acceptatietest')
+});
+
+test('Create New Training', async ({valoriAcademyPlanner}) => { 
+    const GUID = Guid.raw();
+    const responseShow = await valoriAcademyPlanner.CreateTraining('Acceptatietest')
 });
